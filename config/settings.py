7923a79791c401cb587c493d7111b6513cb526a9
dev.heize <dev.heize@Heize-MacBook-Pro.local>
2025-02-02 00:04:41 +0900

    hosting4

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
